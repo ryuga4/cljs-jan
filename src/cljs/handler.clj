5afc20e0bdcf4b71718e93843afd1022b8b8a057
jankun <ryuga4@o2.pl>
2017-07-30 16:40:58 +0200

    better requests

--- src/cljs/handler.clj
+++ src/cljs/handler.clj
@@ -10,37 +10,30 @@
             [ring.util.json-response :refer [json-response]]
             [clojure.data.json :as json]
             ))
-
+(defn toResponse
+  [x]
+  (response (json/read-str (json/write-str x))))
 
 
 (defroutes app-routes
 
   (GET "/" [] (response "I am ready to be created!"))
-  (GET "/users" [] (let [res (json/read-str (json/write-str (map #(dissoc % :_id) (db/total-data))))]
+  (GET "/users" [] (let [res (toResponse (map #(dissoc % :_id :deals) (db/total-data)))]
                      (println res)
-                     (response res)))
+                     res))
 
-  (GET "/test" [dupa] (response (json/write-str {:a "ąąą"})))
   (GET "/send-money" [id1 id2 money] (db/send-money id1 id2 (. Integer parseInt  money))
                                      (response "Przesłane"))
   (GET "/add-friend" [id1 id2] (do (db/add-friend id1 id2)
                                    (response "Dodany znajomy")))
   (GET "/add-user" [id name] (do (db/add-user id name)
                                  (response "Dodany użytkownik")))
-  (POST "/send-money" {:keys [params]} (do (let [{:keys [id id2 money]} params]
-                                             (db/send-money id id2 money))
-                                           (response "Przesłane")))
-  (POST "/add-friend" {:keys [params]} (do (let [{:keys [id1 id2]} params]
-                                             (db/add-friend id1 id2))
-                                           (response "Dodany zn1ajomy")))
-
-  (POST  "/add-user" {:keys [params]} (do (let [{:keys [id name]} params]
-                                            (db/add-user id name))
-                                          (response "Dodany użytkownik")))
 
 
   (GET "/delete-all" [] (do (db/delete-all)
                             (response "Usunięto wszystkich")))
+
+  (GET "/get-one" [id] (toResponse (:deals (db/get-one id))))
   (route/resources "/")
   (route/not-found "not found"))
 
